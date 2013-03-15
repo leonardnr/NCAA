37ccdf6053d310f946965bb58f6c2b7a872dba02
vigilkj <vigilkj@s.dcsdk12.org>
2013-03-15 08:54:25 -0600

    fuck you

---
+++ Documents/Vigil/NCAA/ReadNCAA.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class ReadNCAA {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner reader = new Scanner(new File("ncaa_data.csv"));
+
+		while (reader.hasNextLine()){
+			String next = reader.nextLine();
+			String[] values = next.slpit(",");
+
+			for(String s : values) {
+				System.out.print(s + " ");
+			}
+
+			System.out.println();
+		}
+	}
+}
